45c7a9a47c91949ea03ae02619dd7ab8ac3fd9ff
Rob Weir <rweir@ertius.org>
2014-12-24 23:57:04 +0000

    use docopt

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -1,6 +1,20 @@
+#![feature(phase)]
+
+extern crate docopt;
+extern crate serialize;
+extern crate "rustc-serialize" as rustc_serialize;
+#[phase(plugin)] extern crate docopt_macros;
+
 use std::io::fs;
+use docopt::Docopt;
+
+docopt!(Args deriving Show, "
+Usage: ls [<path>...]
+");
 
 fn main() {
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    println!("Just got {}", args.arg_path);
     let dir = Path::new(".");
     let contents = fs::readdir(&dir);
     match contents {
