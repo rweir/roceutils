daa743c978779255d393470a9ee283ef95598843
Rob Weir <rweir@ertius.org>
2014-12-24 23:18:51 +0000

    actually list files

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -1,3 +1,16 @@
+use std::io::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let dir = Path::new(".");
+    let contents = fs::readdir(&dir);
+    match contents {
+        Ok(results) => { process(results); }
+        Err(e) => ()
+    }
+}
+
+fn process(results: Vec<Path>) {
+    for entry in results.iter() {
+        println!("{}", entry.to_c_str());
+    }
 }
